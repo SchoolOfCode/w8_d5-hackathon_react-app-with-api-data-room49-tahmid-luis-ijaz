c4db660fd94e8efe22224699a91481bf5f98a01e
Tahmid Hussain <TahmidHussain@Tahmids-Mac-mini.local>
2022-01-14 11:50:03 +0000

    part 1 done

--- src/Components/Fetch/index.js
+++ src/Components/Fetch/index.js
@@ -9,17 +9,24 @@ function Fetch() {
         async function fetchData() {
           let response = await fetch(`https://www.superheroapi.com/api.php/5144671915557298/search/${hero}`);
           const data = await response.json();
-          setHoldHero(data.results && data.results[1].id);
+          setHoldHero(data.results && data.results[1]);
           setHero()
           console.log(holdHero)
+          console.log(data.results)
         }
         fetchData();
-      },[hero]); 
+      },[]); 
 
       return(
       <div>
-{/* <h1>{holdHero}</h1> */}
+<h1>{holdHero.name}</h1>
+<h2>{holdHero.id} </h2>
+<img src= "https://www.superherodb.com/pictures2/portraits/10/100/791.jpg"
+alt="superman"/> 
       </div>)
+
     }
 
 export default Fetch 
+
+{/* <img src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name}></img> */}
