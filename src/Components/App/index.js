53edc73edc482fe815b826f769de819520db4d71
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-01-14 21:21:09 +0000

    loading

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import FetchData from '../Fetch';
 import Spiderman from '../Spiderman';
 import Superman from '../Superman';
