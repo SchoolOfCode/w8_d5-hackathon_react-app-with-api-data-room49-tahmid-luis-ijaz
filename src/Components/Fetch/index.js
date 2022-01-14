d9e35abebc6ae00dc8dd1f729d26ce1685acca6e
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-01-14 11:13:03 +0000

    fetch

--- src/Components/Fetch/index.js
+++ src/Components/Fetch/index.js
@@ -2,19 +2,24 @@ import { useEffect, useState } from "react";
 
 
 function Fetch() {
-    const[hero, setHero] = useState("batman") 
-    const [holdHero, setHoldHero] = useState([])
+    const[hero, setHero] = useState("superman") 
+    const [holdHero, setHoldHero] = useState('')
 
     useEffect(() => {
         async function fetchData() {
-          let response = await fetch(`https://www.superheroapi.com/api.php/5144671915557298/search/batman`);
+          let response = await fetch(`https://www.superheroapi.com/api.php/5144671915557298/search/${hero}`);
           const data = await response.json();
-          setHoldHero(data);
+          setHoldHero(data.results);
+          setHero()
           console.log(holdHero)
         }
         fetchData();
-      }, []); 
-      return(<div></div>)
+      },[hero]); 
+
+      return(
+      <div>
+{/* <h1>{holdHero}</h1> */}
+      </div>)
     }
 
 export default Fetch 
